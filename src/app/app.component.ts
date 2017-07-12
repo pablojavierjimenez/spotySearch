c930d92000809dbe5b5e52500e707de853370fa9
pablo javier jimenez <pablojavierjimenez@gmail.com>
2017-07-12 07:06:25 -0300

    adding a standard coments to the generale files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
+/**
+ * Import Depensences
+ * @type {[type]}
+ */
 import { Component, ViewEncapsulation } from '@angular/core';
 
+/**
+ * App component decorator
+ * @param {obj} {  encapsulation [Dependencie]
+ * @example <app></app>
+ */
 @Component({
-  encapsulation: ViewEncapsulation.None,
-  selector: 'app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  encapsulation: ViewEncapsulation.None, // Controler Dependencia
+  selector: 'app', // name of the usable component eg: <app></app>
+  templateUrl: './app.component.html', // Component HTML <app> tag template
+  styleUrls: ['./app.component.less'] // Component CSS
 })
+
+/**
+ * 
+ */
 export class AppComponent {
   public greeting = 'Hello Deloitte!';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
+/**
+ * IMPORT ANGULAR MODULES
+ * here you should import all the angular core modules
+ * that you use or need
+ */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
+/**
+ * IMPORT APP COMPONENTS
+ * here you import all the components created by your own.
+ */
 import { AppComponent } from './app.component';
 import { AppRoutingModule, routedComponents } from './app-routing.module';
 
