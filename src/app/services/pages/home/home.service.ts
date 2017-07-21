0c4886db536880037a4e3e1454ab65b5776b00c0
pablo javier jimenez <pablojavierjimenez@gmail.com>
2017-07-21 07:48:22 -0300

    adding a first service

---
+++ src/app/services/pages/home/home.service.ts
@@ -0,0 +1,15 @@
+/**
+ * ${componentName} Service Imports
+ * Component TagName: HeroService
+ */
+import { Injectable } from '@angular/core';
+
+
+/**
+ * ${componentName} Injectable Configuration
+ * Injectable TagName: HeroService
+ */
+@Injectable()
+
+export class HeroService {
+}
